7b210c6e0d3f6e995eafeccc240074b741fa2ffe
Pranjal Goyal <id4pranjal@gmail.com>
2023-07-30 00:08:17 +0530

    chenged button of the website

--- src/components/Section/Intro/Intro.jsx
+++ src/components/Section/Intro/Intro.jsx
@@ -22,9 +22,9 @@ const Intro = () => {
           scalable backend.
         </p>
       </div>
-      <button className="btn">
+      <div className="btn inline-flex h-12 animate-background-shine items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-300 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
         <a href="#hello-p">Say Hello</a>
-      </button>
+      </div>
       <i className="fab fa-react icon"></i>
       <i className="fab fa-chrome icon"></i>
       <i className="far fa-file-code icon"></i>
--- src/components/compounds/Navbar/Navbar.jsx
+++ src/components/compounds/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
           <NavItem href="#hello-p" number="05." title="Contact" />
 
           <li>
-            <button className="btn">
+            <button className="btn inline-flex h-12 animate-background-shine items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <a className="" href="./pdfs/cv.pdf" download="Pranjal CV">
                 Resume
               </a>
